5a2045376207df7b1ff623e39f85f772be00ebc8
rudr1gu <rodrigo.santos.ii@hotmail.com>
2024-05-21 21:56:04 -0300

    Java Spring

---
+++ src/test/java/com/example/demonstracao/DemonstracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demonstracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
